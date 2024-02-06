1a1f4ee8e29f05ae7b410512d0f44d8ff8945c7f
Tom Dent <tomdent991@hotmail.com>
2024-02-06 21:03:27 +0100

    changed image opt

--- components/ImageComponent/ImageComponent.js
+++ components/ImageComponent/ImageComponent.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ImageComponent({ image }) {
   return (
     <div>
-      <Image src={image.src} alt="uploaded images" width={100} height={150} priority/>
+      <Image src={image.src} alt="uploaded images" width={50} height={90} priority/>
     </div>
   );
 }
