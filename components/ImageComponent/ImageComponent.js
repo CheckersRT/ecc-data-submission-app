f1db43cb987b6c2d657e2904404e1963562510cd
Tom Dent <tomdent991@hotmail.com>
2024-02-07 14:38:43 +0100

    removed next Image optiimisation

--- components/ImageComponent/ImageComponent.js
+++ components/ImageComponent/ImageComponent.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function ImageComponent({ image }) {
-
-
   return (
     <div>
-      <Image src={image.src} alt="uploaded images" fill={true} width={100} height={150} priority/>
+      <img
+        src={image.src}
+        alt="uploaded images"
+        // fill={true}
+        // width={150}
+        // height={150}
+        // priority
+      />
     </div>
   );
 }
