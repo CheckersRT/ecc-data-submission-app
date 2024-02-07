86bac925e0e7ab2f75b5458638dd926a83306d6c
Tom Dent <tomdent991@hotmail.com>
2024-02-07 14:27:20 +0100

    add fill true to image component

--- components/ImageComponent/ImageComponent.js
+++ components/ImageComponent/ImageComponent.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function ImageComponent({ image }) {
+
+
   return (
     <div>
-      <Image src={image.src} alt="uploaded images" width={100} height={150} priority/>
+      <Image src={image.src} alt="uploaded images" fill={true} width={100} height={150} priority/>
     </div>
   );
 }
